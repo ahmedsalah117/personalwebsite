9025b8381700fd08bd1db35bf838b98ae2fb7d73
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2023-10-15 20:19:35 +0300

    update vite config again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.PNG", "**/*.JPG"],
+  assetsInclude: ["**/*.PNG", "**/*.JPG", "**/*.png", "**/*.jpg"],
   base: "/personalwebsite",
 });
