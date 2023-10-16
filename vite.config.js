582fc0df1b1b8486468b159b2025a26c8fd6b4b9
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2023-10-16 14:49:39 +0300

    deploy attempt

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.PNG", "**/*.JPG", "**/*.png", "**/*.jpg"],
-  base: "/personalwebsite",
+  base: "/personalwebsite/",
 });
